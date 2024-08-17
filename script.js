594a9a122e5c25511f2a0b1763f204cab1392857
Harsha Prabha Nataraj <158758099+harshaprabha874n@users.noreply.github.com>
2024-08-17 22:45:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let outputScreen=document.getElementById("output-screen");
+function display(num)
+{
+    outputScreen.value +=num;
+}
+function calculate(){
+    try{
+        outputScreen.value=eval(outputScreen.value);
+    }
+    catch(err)
+    {
+        alert("Invalid");
+    }
+}
+function clr(){
+    outputScreen.value= "";
+}
+function del(){
+    outputScreen.value= outputScreen.value.slice(0,-1);}
